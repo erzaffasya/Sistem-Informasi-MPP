18ed0be2630cdd734fb0e7509a53b25c69e45697
Muhammad ilham <muhilham0603@gmail.com>
2023-07-16 00:53:48 +0800

    FE BARU

--- public/tlandingpage/js/script.js
+++ public/tlandingpage/js/script.js
@@ -1,11 +1,41 @@
 $('.slick-layanan').slick({
   infinite: true,
   slidesToShow: 4,
-  slidesToScroll: 2,
+  slidesToScroll: 1,
   arrows: true,
   // adaptiveHeight: true,
   autoplay: true,
   autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 $('.slick-testimoni').slick({
   infinite: true,
@@ -13,6 +43,13 @@ $('.slick-testimoni').slick({
   slidesToScroll: 1,
   arrows: true,
 });
+
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
 
 // Preloader js
 $(window).on("load", function () {
